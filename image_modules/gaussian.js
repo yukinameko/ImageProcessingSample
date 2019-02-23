3ad7febf34b51e7610ed98a827b9ef7566df0625
yukinameko <g1813551@gmail.com>
2019-02-24 04:13:23 +0900

    [fix] gaussian.jsのカーネルサイズの偶奇判定処理の修正

--- image_modules/gaussian.js
+++ image_modules/gaussian.js
@@ -1,6 +1,7 @@
 const math = require('mathjs');
 
 const conv = require('../image_modules/convolution.js');
+const map = require('../image_modules/map.js');
 
 module.exports = (img, {kSize=0, sigma=0} = {kSize:5, sigma:1}) => {
 	let kSizeHalf = 0;
@@ -8,13 +9,18 @@ module.exports = (img, {kSize=0, sigma=0} = {kSize:5, sigma:1}) => {
 		kSizeHalf = parseInt(sigma*2+0.5);
 		kSize = kSizeHalf*2+1;
 	}else if(sigma == 0){
-		if(kSize|1 == 0)kSize++;
+		if((kSize&1) == 0)kSize++;
 		kSizeHalf = (kSize-1)/2;
 		sigma = kSizeHalf/2;
 	}
-	const gaussianFilter = Array.from(new Array(kSize)).map((_, j) =>
+	const gFilter = Array.from(new Array(kSize)).map((_, j) =>
 		Array.from(new Array(kSize)).map((_, i) => {
-			return math.exp(-(j*j+i*i)/(2/sigma/sigma))/(2*math.PI*sigma*sigma);
+			const j_ = j-kSizeHalf;
+			const i_ = i-kSizeHalf;
+			return math.exp(-(j_*j_+i_*i_)/(2/sigma/sigma))/(2*math.PI*sigma*sigma);
 		}));
+	const sum = gFilter.reduce((pre, cur) => pre+cur.reduce((pre, cur) => pre+cur, 0), 0);
+	const gaussianFilter = map(gFilter, v => v/sum);
+
 	return conv.conv(img, [img.length, img[0].length], gaussianFilter, [kSize,kSize], {mode:conv.EXPAND});
 }
