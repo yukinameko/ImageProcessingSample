24558710b9f0616776a18e78dd161921e7891746
yukinameko <g1813551@gmail.com>
2019-02-24 04:43:15 +0900

    [fix] diff.jsのコマンドライン引数処理の修正

--- diff.js
+++ diff.js
@@ -3,13 +3,13 @@ const fs = require('fs');
 
 const argv = process.argv.slice(2);
 
-if(process.argv.length < 2){
+if(argv.length < 2){
 	console.log('error: not found argv'+argv.length+'\nnode diff <input image1 path> <input image2 path>');
 	process.exit(1);
 }
 
-const img1 = cv.imread(process.argv[0], cv.CV_8UC1);
-const img2 = cv.imread(process.argv[1], cv.CV_8UC1);
+const img1 = cv.imread(argv[0], cv.CV_8UC1);
+const img2 = cv.imread(argv[1], cv.CV_8UC1);
 
 if(img1.sizes.toString() != img2.sizes.toString()){
 	console.log('error: size is different');
